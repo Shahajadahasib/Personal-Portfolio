5539d04e774c96c30c9fc98d05508705ddcc488f
Shahajadahasib <shahajadahasib@gmail.com>
2022-03-20 22:23:34 +0600

    bugs fixing

---
+++ main.js
@@ -0,0 +1,7 @@
+// typed Animation script
+var typed = new Typed(".typing", {
+    strings: ["YouTuber", "Developer", "Bloger", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
