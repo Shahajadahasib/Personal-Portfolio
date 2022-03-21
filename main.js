6d11908b477b5b686ce01b2aca4ec33d69904b6e
Shahajadahasib <shahajadahasib@gmail.com>
2022-03-21 22:42:58 +0600

    nav

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ var typed = new Typed(".typing", {
     backSpeed: 60,
     loop: true
 });
+
+window.addEventListener("scroll", function() {
+    var nav = document.getElementById("nav");
+    nav.classList.toggle("sticky", window.scrollY > 0)
+
+
+})
